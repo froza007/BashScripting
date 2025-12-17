28cb116a7546e9d18441b30ee686cc64ce8f0833
froza007 <barath282003@gmail.com>
2025-12-17 10:44:02 +0530

    Updated

---
+++ robot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello $LOGNAME"
+echo "Welcome to Automation"
+echo "The Host Name is `hostname`"
+echo "Your present working directory is `pwd`"
+
+echo "Thank you, see you soon $LOGNAME"
+
+
